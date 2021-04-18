ded63df4da590751d4da87e2ec9a64d873647060
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-04-18 16:13:33 +0530

    binary_converter

---
+++ myPersonalProject/src/binaryConverter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class binaryConverter {
+    public static void main(String[ ] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.print(Converter.toBinary(x));
+    }
+    
+}
+
+ class Converter{
+    public static String toBinary(int num){
+
+        //binary conversion goes here...
+        String binary="";
+        while(num > 0) {
+        binary = (num%2)+binary;
+        num /= 2;
+       
+       }
+        return binary;
+
+    }
+}
+
