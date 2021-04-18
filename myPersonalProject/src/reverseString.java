caa07e4ad91814c245bbe3b6f1f46cd6066c4070
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-04-18 12:57:32 +0530

    reverse_string

---
+++ myPersonalProject/src/reverseString.java
@@ -0,0 +1,17 @@
+package myPersonalProject.src;
+import java.util.Scanner;
+
+public class reverseString {
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         String text=scanner.nextLine();
+         char[] arr = text.toCharArray();
+         char[] newarr=new char[arr.length];
+
+         for(int i=arr.length,j=0;i>0;i--,j++){
+           newarr[j]=arr[i-1];
+
+         }
+         System.out.println(newarr);
+     }    
+}
