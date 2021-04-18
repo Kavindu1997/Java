cf06cd4d1850a05e9c5516e4b671e6046a067cf5
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-04-18 12:03:49 +0530

    loan_converter

---
+++ myPersonalProject/src/loanConverter.java
@@ -0,0 +1,18 @@
+package myPersonalProject.src;
+import java.util.Scanner;
+
+public class loanConverter {
+
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int amount = scanner.nextInt();
+
+		for(int i=1;i<=3;i++){
+			int pay=amount/10;
+			amount-=pay;			
+		}
+		System.out.println(amount);
+		
+	}
+    
+}
